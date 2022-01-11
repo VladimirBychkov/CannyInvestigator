33c414cb6ba139535261a9ed833ebbbd13114f46
Vladimir Bychkov <bychkov.vladimir@gmail.com>
2022-01-10 18:29:52 -0700

    adding local otsu

--- analyzers/otsu.py
+++ analyzers/otsu.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 import numpy as np
-from skimage.filters import threshold_otsu
+from skimage.morphology import disk, diamond, rectangle
+from skimage.filters import threshold_otsu, rank
 from PIL import Image
 
 
@@ -12,7 +13,20 @@ def get_layout():
                 sg.Checkbox("Green", default=True, key='-OTSU GREEN CHANNEL-'),
                 sg.Checkbox("Blue", default=True, key='-OTSU BLUE CHANNEL-'),
             ]
-        ], title="Otsu Channels")],
+        ], title="Channels")],
+        [
+            sg.Checkbox("Local Otsu", default=False, key="-APPLY LOCAL OTSU-"),
+        ],
+        [sg.Frame(layout=[
+            [
+                sg.Text("Size"),
+                sg.Slider((1, 150), 1, 1, orientation="h", size=(40, 15), key="-LOCAL OTSU SIZE-"),
+            ],
+            [
+                sg.Text("Local Shape"),
+                sg.InputCombo(("Disk", "Rect"), default_value="Disk", enable_events=True, key="-LOCAL OTSU SHAPE-", size=(10, 15)),
+            ],
+        ], title="Local Otsu Parameter")],
     ]
 
     return layout
@@ -23,9 +37,15 @@ def process_image(pil_image, values):
     selected_channels = [values["-OTSU RED CHANNEL-"], values["-OTSU GREEN CHANNEL-"], values["-OTSU BLUE CHANNEL-"]]
 
     if np.where(selected_channels)[0].size == 1:
-        threshold_global_otsu = threshold_otsu(img[..., np.where(selected_channels)[0][0]])
-        global_otsu = img[..., np.where(selected_channels)[0][0]] >= threshold_global_otsu
-        ret_img = Image.fromarray(255 * np.asarray(global_otsu, dtype=np.uint8))
+        if values["-APPLY LOCAL OTSU-"]:
+            radius = values["-LOCAL OTSU SIZE-"]
+            local_mask = disk(radius) if values["-LOCAL OTSU SHAPE-"] == "Disk" else rectangle(int(radius), int(radius))
+            local_otsu = img[..., np.where(selected_channels)[0][0]] >= rank.otsu(img[..., np.where(selected_channels)[0][0]], local_mask)
+            ret_img = Image.fromarray(255 * np.asarray(local_otsu, dtype=np.uint8))
+        else:
+            threshold_global_otsu = threshold_otsu(img[..., np.where(selected_channels)[0][0]])
+            global_otsu = img[..., np.where(selected_channels)[0][0]] >= threshold_global_otsu
+            ret_img = Image.fromarray(255 * np.asarray(global_otsu, dtype=np.uint8))
     else:
         ret_img = pil_image
 
