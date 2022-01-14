3e0aee1da9b44cef65c7e1e87f568451f08ffdfe
Vladimir Bychkov <bychkov.vladimir@gmail.com>
2022-01-13 21:30:04 -0700

    overlay otsu with original image to see segmentation edges

--- analyzers/otsu.py
+++ analyzers/otsu.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import numpy as np
+import cv2.cv2 as cv2
 from skimage.morphology import disk, diamond, rectangle
 from skimage.filters import threshold_otsu, rank
 from PIL import Image
@@ -16,6 +17,7 @@ def get_layout():
         ], title="Channels")],
         [
             sg.Checkbox("Local Otsu", default=False, key="-APPLY LOCAL OTSU-"),
+            sg.Checkbox("Overlay with Image", default=False, key="-OTSU OVERLAY WITH IMAGE-"),
         ],
         [sg.Frame(layout=[
             [
@@ -40,21 +42,33 @@ def process_image(pil_image, values):
         if values["-APPLY LOCAL OTSU-"]:
             radius = values["-LOCAL OTSU SIZE-"]
             local_mask = disk(radius) if values["-LOCAL OTSU SHAPE-"] == "Disk" else rectangle(int(radius), int(radius))
-            local_otsu = img >= rank.otsu(img, local_mask)
-            ret_img = Image.fromarray(255 * np.asarray(local_otsu, dtype=np.uint8))
+            otsu_img = img >= rank.otsu(img, local_mask)
         else:
-            global_otsu = img >= threshold_otsu(img)
-            ret_img = Image.fromarray(255 * np.asarray(global_otsu, dtype=np.uint8))
+            otsu_img = img >= threshold_otsu(img)
+
+        otsu_np_img = 255 * np.asarray(otsu_img, dtype=np.uint8)
+        if values["-OTSU OVERLAY WITH IMAGE-"]:
+            cnts = cv2.findContours(otsu_np_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+            cv2.drawContours(img, cnts, -1, 255, 1)
+            otsu_np_img = img
+        ret_img = Image.fromarray(otsu_np_img)
+
     elif np.where(selected_channels)[0].size == 1:
         if values["-APPLY LOCAL OTSU-"]:
             radius = values["-LOCAL OTSU SIZE-"]
             local_mask = disk(radius) if values["-LOCAL OTSU SHAPE-"] == "Disk" else rectangle(int(radius), int(radius))
-            local_otsu = img[..., np.where(selected_channels)[0][0]] >= rank.otsu(img[..., np.where(selected_channels)[0][0]], local_mask)
-            ret_img = Image.fromarray(255 * np.asarray(local_otsu, dtype=np.uint8))
+            otsu_img = img[..., np.where(selected_channels)[0][0]] >= rank.otsu(img[..., np.where(selected_channels)[0][0]], local_mask)
         else:
             threshold_global_otsu = threshold_otsu(img[..., np.where(selected_channels)[0][0]])
-            global_otsu = img[..., np.where(selected_channels)[0][0]] >= threshold_global_otsu
-            ret_img = Image.fromarray(255 * np.asarray(global_otsu, dtype=np.uint8))
+            otsu_img = img[..., np.where(selected_channels)[0][0]] >= threshold_global_otsu
+
+        otsu_np_img = 255 * np.asarray(otsu_img, dtype=np.uint8)
+        if values["-OTSU OVERLAY WITH IMAGE-"]:
+            cnts = cv2.findContours(otsu_np_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]
+            cv2.drawContours(img, cnts, -1, 255, 1)
+            otsu_np_img = img
+        ret_img = Image.fromarray(otsu_np_img)
+
     else:
         ret_img = pil_image
 
